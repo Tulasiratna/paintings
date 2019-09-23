aa16a315bff8988980102a7e689f4aa5c355075f
Handan Zhang <hmix.cc@gmail.com>
2019-09-23 16:29:53 +0200

    added devise gem to the project

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # define default url in development environment.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
