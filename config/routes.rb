00768585373d81c27bf6a7ea0faba4eeef71174e
Handan Zhang <hmix.cc@gmail.com>
2019-09-24 11:16:54 +0200

    added routes for paintings

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'paintings/destroy'
-  get 'paintings/new'
-  get 'paintings/create'
-  get 'paintings/edit'
-  get 'paintings/update'
-  get 'users/index'
-  get 'users/show'
+  resources :paintings
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
