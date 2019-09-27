c0efbd345484aa90cf692afa3285b7456d534371
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-09-27 14:51:03 +0200

    done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,33 +14,26 @@ class BookingsController < ApplicationController
     @booking.user = current_user
 
     if @booking.save
+      flash[:notice] = "Your painting has been booked"
+
       redirect_to paintings_path
     else
+      flash[:error] = "Booking unsuccessful"
+
       render 'paintings/show'
     end
   end
 
   def confirm
     @booking.confirmed = true
-      # @booking.confirm#
-      # flash[:success] = "Your painting has been booked"
-      # @booking.update_attributes!(booking_number: "MAMA" + '- ' + SecureRandom.hex(4).upcase)
     if @booking.save
-      alert[:success] = "Your painting has been booked"
+      #alert[:success] = "Your painting has been booked"
       redirect_to painting_path(@painting)
     else
-      flash[:error] = "Booking unsuccessful"
+      #flash[:error] = "Booking unsuccessful"
       render "new"
     end
   end
-
-  # 1 - create a new action called confirm
-  # 2 - does the current user own the painting
-  # 3 - change the state of the booking
-  # 4 - save
-  # 5-  if save redirect to booking page
-  # 6 - have a flash message
-
   private
 
   def booking_params
